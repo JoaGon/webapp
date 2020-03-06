8f3dd2c5b25d1169525e45fe76fef1a754f23647
Joanelly <joanellygonzalez@gmail.com>
2020-03-06 03:03:20 -0500

    display mock data

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,21 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { getUser, removeUserSession } from '../../utils/common';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-function Dashboard(props) {
+class Dashboard extends Component {
 
-    const user = getUser();
-    // handle click event of logout button
-    const handleLogout = () => {
-        removeUserSession();
-        props.history.push('/login');
+
+    componentWillMount() {
+        this.user = getUser();
     }
 
-    return (
-        <div>
-            Welcome User!<br /><br />
-            <input type="button" onClick={handleLogout} value="Logout" />
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            columnDefs: [{
+                headerName: "BookingId", field: "bookingId", sortable: true
+            }, {
+                headerName: "Cliente", field: "Cliente", sortable: false
+            }, {
+                headerName: "Fecha de Creación", field: "bookingTime", sortable: false
+            }, {
+                headerName: "Dirección", field: "streetAddress", sortable: false
+            }, {
+                headerName: "Precio", field: "bookingPrice", sortable: true
+            }
+
+            ],
+            rowData: [{
+                bookingId: "1", Cliente: "Cliente", bookingTime: 35000, streetAddress: 'test', bookingPrice: '500'
+            }, {
+
+                bookingId: "5", Cliente: "Cliente", bookingTime: 35000, streetAddress: 'test', bookingPrice: '100'
+            }, {
+
+                bookingId: "25", Cliente: "Cliente", bookingTime: 35000, streetAddress: 'test', bookingPrice: '650'
+            }, {
+
+                bookingId: "3", Cliente: "Cliente", bookingTime: 35000, streetAddress: 'test', bookingPrice: '10'
+            }, {
+
+                bookingId: "6", Cliente: "Cliente", bookingTime: 35000, streetAddress: 'test', bookingPrice: '15'
+            }
+            ],
+            user: ""
+        }
+        this.handleLogout = this.handleLogout.bind(this);
+
+    }
+    // handle click event of logout button
+    handleLogout = () => {
+        removeUserSession();
+        //props.history.push('/login');
+    }
+
+
+    render() {
+        return (
+            <div>
+                Welcome User!<br /><br />
+                <div
+                    className="ag-theme-balham"
+                    style={{
+                        height: '500px',
+                        width: '1200px'
+                    }}
+                >
+                    <AgGridReact
+                        columnDefs={this.state.columnDefs}
+                        rowData={this.state.rowData}>
+                    </AgGridReact>
+                </div>
+                <button onClick={e => { e.preventDefault(); this.handleLogout() }}>
+                    Logout
+            </button>
+            </div>
+        );
+    }
 }
+
+
 
 export default Dashboard;
